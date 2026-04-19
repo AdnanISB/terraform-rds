8c649f22292f8c55676cc02aeef98bd9dac6d18c
adnan <126405134+AdnanISB@users.noreply.github.com>
2026-04-19 18:17:51 +0000

    files added

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,14 +1,14 @@
 output "rds_endpoint" { 
   description = "RDS database endpoint" 
-  value       = aws_db_instance.this.endpoint 
+  value       = aws_db_instance.mysql
 } 
  
 output "rds_identifier" { 
   description = "RDS instance identifier" 
-  value       = aws_db_instance.this.id 
+  value       = aws_db_instance.mysql.id 
 } 
  
 output "rds_security_group_id" { 
   description = "Security group ID attached to RDS" 
-  value       = aws_security_group.this.id 
+  value       = aws_security_group.rds_sg.id 
 }
